77d2275bd044e9e22b4150a70d2cce9428b0a3ce
moatkon <moatkon@gmail.com>
2025-06-24 00:12:49 +0800

    init maven

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ai-assistant
 个人AI助手
+
+
+mvn archetype:generate -DgroupId=com.moatkon.assistant -DartifactId=ai-assistant -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
---
+++ assistant-boostrap/src/main/java/com/moatkon/assistant/AssistantBootstrap.java
@@ -0,0 +1,11 @@
+package com.moatkon.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantBootstrap.class, args);
+    }
+}
