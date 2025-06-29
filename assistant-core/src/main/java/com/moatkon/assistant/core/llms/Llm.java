becf3dd35cbe533eb9b59717759b260726f65360
moatkon <moatkon@gmail.com>
2025-06-29 19:05:50 +0800

    添加LLM和Prompt抽象

---
+++ assistant-core/src/main/java/com/moatkon/assistant/core/llms/Llm.java
@@ -0,0 +1,9 @@
+package com.moatkon.assistant.core.llms;
+
+/**
+ * @author Yang Runkang (cruise)
+ * @createTime 2025-06-29 19:04
+ * @email: yangrunkang53@gmail.com
+ */
+public abstract class Llm {
+}
---
+++ assistant-core/src/main/java/com/moatkon/assistant/core/prompts/Prompt.java
@@ -0,0 +1,9 @@
+package com.moatkon.assistant.core.prompts;
+
+/**
+ * @author Yang Runkang (cruise)
+ * @createTime 2025-06-29 19:05
+ * @email: yangrunkang53@gmail.com
+ */
+public abstract class Prompt {
+}
