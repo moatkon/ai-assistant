333d34d5f6b4adb2cda7122ca13e1cd09009719e
moatkon <moatkon@gmail.com>
2025-06-29 19:04:19 +0800

    添加工具和代理抽象

---
+++ assistant-core/src/main/java/com/moatkon/assistant/core/agents/Agent.java
@@ -0,0 +1,9 @@
+package com.moatkon.assistant.core.agents;
+
+/**
+ * @author Yang Runkang (cruise)
+ * @createTime 2025-06-29 19:03
+ * @email: yangrunkang53@gmail.com
+ */
+public abstract class Agent {
+}
